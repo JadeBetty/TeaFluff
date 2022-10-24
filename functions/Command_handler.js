ad98adc04557fd35bfcbfa8cc2bffb346381021c
unknown <ngynag@gmail.com>
2022-10-24 12:20:09 +0700

    Some changes

--- functions/Command_handler.js
+++ functions/Command_handler.js
@@ -1,8 +1,25 @@
 const fs = require('fs');
 module.exports = (client, Discord) => {
+    let command_folders = fr.readdirSync("./Commands").forEach(folder => {
+        const command_files = fs.readdirSync("./Commands").filter(file => file.endsWith('.js'));
+        command_files.forEach(f => {
+            const command = require(`../Commands/${folder}/${f}`)
+            client.commands.set(command.name, command)
+            if(command.aliases) {
+                command.aliases.forEach(alias => {
+                    client.aliases.set(alias, command)
+                })
+            } else {
+                continue;
+            }
+        })
+    })
+    console.log(`Successfully loaded ${client.commands.size} commands!`)
+    
+    /*
     const command_files = fs.readdirSync("./Commands").filter(file => file.endsWith('.js'));
     for(const file of command_files){
-        const command = require(`../Commands/${file}`);
+        const command = require(`../Commands/${folder}`);
         if(command.name){
             client.commands.set(command.name, command);
             if (command.aliases) {
@@ -13,5 +30,5 @@ module.exports = (client, Discord) => {
             continue;
         }
     }
-}
+} */
 }
