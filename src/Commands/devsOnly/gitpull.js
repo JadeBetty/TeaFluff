c439ad845a56e7a02101eee08e320eb3b1993c67
unknown <ngynag@gmail.com>
2022-11-08 09:41:08 +0700

    Added reload and git pull command, still in beta

---
+++ src/Commands/devsOnly/gitpull.js
@@ -0,0 +1,32 @@
+const {exec} = require('child_process');
+const {EmbedBuilder} = require('discord.js');
+
+const { devs } = require("../../../config.json")
+module.exports = {
+  name: 'gitpull',
+  description: 'Pull latest code from github.',
+  aliases: ['gp'],
+  disabledChannel: [],
+  category: 'Owner',
+
+  run: async (client, message, args) => {
+    if (devs.includes(message.author.id)) {
+      const command = 'git pull';
+      exec(command, async (err, stdout, stderr) => {
+        if (err) return console.log(err);
+        let res = stdout || stderr;
+        message.channel.send({
+          embeds: [
+            new EmbedBuilder()
+              .setTitle('Git Pull')
+              .setColor('#171515')
+              .setDescription(`\`\`\`js\n${res.slice(0, 2000)}\n\`\`\``),
+          ],
+        });
+      });
+    } else
+      return message.reply(
+        'Only the developers of cheeku can run this command.',
+      );
+  },
+};
---
+++ src/Commands/devsOnly/reload.js
@@ -0,0 +1,42 @@
+const {EmbedBuilder} = require('discord.js');
+const path = require('path');
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+module.exports = {
+  name: 'reload',
+  category: 'Owner',
+  devOnly: true,
+  description: 'Reload all the events and commands.',
+  aliases: ['reloadall', 'restart'],
+  disabledChannel: [],
+  run: async (client, message, args) => {
+    let msg = await message.channel.send({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle('Restarting...')
+          .setDescription('The bot is now restarting.')
+          .setColor('Orange'),
+      ],
+    });
+    setTimeout(async () => {
+      await msg.edit({
+        embeds: [
+          new EmbedBuilder()
+            .setTitle('Stopped!')
+            .setDescription('The bot has finally completed stopped.')
+            .setColor('Red')
+            .setFooter({
+              text: 'The bot shall be back within 10-20 seconds.',
+            }),
+        ],
+      });
+      // Create a temporary file called "restart.txt" and write the message id, message channel, guild channel to it
+      const file = path.join(__dirname, '../../../restart.txt');
+      // id, channel id, guild id and current time must be written to the file
+      let data = `${msg.id},${msg.channel.id},${msg.guild.id},${Date.now()}`;
+      fs.writeFileSync(file, data);
+      exec('pkill -f -SIGHUP nodemon');
+    }, 10000);
+  },
+}
