9ea9e5a3a1695b718b25af24c0cf19078008a08e
unknown <ngynag@gmail.com>
2022-11-26 20:42:55 +0700

    Fixes in accept-suggestion and added deny-suggestion command

--- src/Commands/suggestion/deny-suggestion.js
+++ src/Commands/suggestion/deny-suggestion.js
@@ -1,42 +1,47 @@
 const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    name: `accept-suggestion`,
-    description: "accept a suggestion!",
+    name: `deny-suggestion`,
+    description: "Deny a suggestion!",
+    aliases: ["deny-suggest", "d-sug"],
     permissions: ["ManageMessages"],
-    aliases: ["ac-sug",],
     category: "Suggestion",
     run: async (client, message, args) => {
         const messageID = args[0];
         const reason = args.slice(1).join(' ') || 'No reason given';
-
         const suggestionChannel = await message.guild.channels.fetch(
             '1045869650287796225',
         );
-        if (!messageID) return message.reply('Please provide a message ID');
         const suggestedMsg = await suggestionChannel.messages.fetch(messageID);
-        const suggestEmbed = suggestedMsg.embeds[0]
-        let acceptedEmbed = new EmbedBuilder()
+        const suggestEmbed = suggestedMsg.embeds[0];
+
+        if (!messageID) return message.reply('Please provide a message ID');
+        let deniedEmbed = new EmbedBuilder()
             .setAuthor({
                 name: suggestEmbed.author.name,
                 iconURL: suggestEmbed.author.iconURL,
             })
+
             .setTitle(suggestEmbed.title)
             .setDescription(suggestEmbed.description)
-            .setColor("Green")
-            .addFields({ name: `Status:`, value: `🟢 Nice idea! We're working on it!\n\n**Reason:** ${reason}` })
+            .setColor("Red")
+            .addFields({
+                name: `Status`,
+                value: `🔴 Thank you for your suggestion, but the community doesn't seem to be interested in it for now\n\n**Reason:** ${reason}`
+            })
             .setFooter({
-                text: `accepted by: ${message.author.tag}`,
+                text: `denied by: ${message.author.tag}`,
                 iconURL: `${message.author.displayAvatarURL({ dynamic: true })}`,
             });
+
         try {
-            await suggestedMsg.edit({ embeds: [acceptedEmbed] });
-            await message.reply('Suggestion accepted!');
+            await suggestedMsg.edit({ embeds: [deniedEmbed] });
+            await message.reply('Suggestion denied!');
         } catch (err) {
             console.log(err);
             await message.reply(`\`\`\`${err}\`\`\``);
         }
 
-
     }
+
 }
