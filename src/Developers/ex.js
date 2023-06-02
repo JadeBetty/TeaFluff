8278224f58812b8cb280f25a7f7b3d265fc325b1
JadeBetty <ngynag@gmail.com>
2023-06-02 09:44:22 +0700

    new version with compile command beta 2.4.0

--- src/Developers/ex.js
+++ src/Developers/ex.js
@@ -4,7 +4,8 @@ module.exports = {
     name: "ex",
     run: async (client, message, args) => {
         const axios = require('axios');
-        message.channel.send(`[a](https://youtube.com)`)
+        message.channel.send(`[a](https://youtube.com)`);
+        axios.get("https://discordjs.guide").then(re => console.log(re))
         axios.post('https://wandbox.org/api/compile.json', {
             code: 'console.log("Hello, World!");',
             compiler: 'nodejs-16.14.0',
@@ -19,42 +20,58 @@ module.exports = {
             axios.get('https://wandbox.org/api/list.json')
             .then(response => {
               // handle the response data here
+              const array = response.data
+              
+              const latestVersions = Object.values(array.reduce((accumulator, currentValue) => {
+                if (accumulator[currentValue.language]) {
+                  if (currentValue.version > accumulator[currentValue.language].version) {
+                    accumulator[currentValue.language] = currentValue;
+                  }
+                } else {
+                  accumulator[currentValue.language] = currentValue;
+                }
+                return accumulator;
+              }, {}));
+              
+              
+
               const attachment = new Discord.AttachmentBuilder(
-                Buffer.from(response.data.map(a => a.name).join("\n"), "utf-8"), {name: "amongus.txt"}
+                Buffer.from(latestVersions, "utf-8"), {name: "amongus.txt"}
             )
-              return message.channel.send({files: [attachment]})
+               message.channel.send({files: [attachment]})
+              message.channel.send(latestVersions.map(a => a.language).join(", "))
             })
             .catch(error => {
               // handle the error here
               console.log(error);
             });
-        const promotionsChannels = [
-            "936242386319863880",
-            "936242343277912074",
-            "936242506469871626"
-        ]
-        if (!promotionsChannels.includes(message.channel.id)) {
-            const messages = await message.channel.messages.fetch({ limit: 10 })
-            let count = 0;
-            messages.forEach(msg => {
-                if (msg.author.id !== client.user.id) {
-                    count++;
-                }
-            })
-            if (count < 10) {
-                message.delete();
-                return message.author.send({
-                    embeds: [
-                        new Discord.EmbedBuilder()
-                            .setTitle("your title or you can remove it")
-                            .setDescription("aaa")
-                            .setColor("White")
-                    ]
-                })
-            } else {
-                count = 0;
-            }
-        }
+        // const promotionsChannels = [
+        //     "936242386319863880",
+        //     "936242343277912074",
+        //     "936242506469871626"
+        // ]
+        // if (!promotionsChannels.includes(message.channel.id)) {
+        //     const messages = await message.channel.messages.fetch({ limit: 10 })
+        //     let count = 0;
+        //     messages.forEach(msg => {
+        //         if (msg.author.id !== client.user.id) {
+        //             count++;
+        //         }
+        //     })
+        //     if (count < 10) {
+        //         message.delete();
+        //         return message.author.send({
+        //             embeds: [
+        //                 new Discord.EmbedBuilder()
+        //                     .setTitle("your title or you can remove it")
+        //                     .setDescription("aaa")
+        //                     .setColor("White")
+        //             ]
+        //         })
+        //     } else {
+        //         count = 0;
+        //     }
+        // }
 
     }
 }
